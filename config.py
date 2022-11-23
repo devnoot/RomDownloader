cef3a7efc4d951cbb6502afb9f21b042d1e94506
noot <devnoot@gmail.com>
2022-11-22 23:38:06 -0500

    Add config

---
+++ config.py
@@ -0,0 +1,46 @@
+config = {
+    'gc': [
+        'https://archive.org/download/RedumpNintendoGameCubeAmerica',
+        'https://archive.org/download/RedumpNintendoGameCubeAmericaPart2',
+        'https://archive.org/download/RedumpNintendoGameCubeAmericaPart3'
+    ],
+    'psx': [
+        'https://archive.org/download/chd_psx/CHD-PSX-USA/'
+    ],
+    'nes': [
+        'https://archive.org/download/nointro.nes'
+    ],
+    'snes': [
+        'https://archive.org/download/nointro.snes'
+    ],
+    'vb': [
+        'https://archive.org/download/nointro.vb'
+    ],
+    'gb': [
+        'https://archive.org/download/nointro.gb'
+    ],
+    'gbc': [
+        'https://archive.org/download/nointro.gbc'
+    ],
+    'gba': [
+        'https://archive.org/download/nointro.gba'
+    ],
+    'n64': [
+        'https://archive.org/download/nointro.n64'
+    ],
+    'ms-mkiii': [
+        'https://archive.org/download/nointro.ms-mkiii'
+    ],
+    'md': [
+        'https://archive.org/download/nointro.md'
+    ],
+    'gg': [
+        'https://archive.org/download/nointro.gg'
+    ],
+    '32x': [
+        'https://archive.org/download/nointro.32x'
+    ],
+    'tg-16': [
+        'https://archive.org/download/nointro.tg-16'
+    ]
+}
